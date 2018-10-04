e04b000d1cde1159e4ce2fde271a00cd45f92ff7
Igor Beliaev <slipstak2@yandex.ru>
2018-10-04 22:47:41 +0300

    add ESC keyboard-callback refactoring vcxproj

--- cgsg2018/src/CGC2018_00_Intro/CGC2018_00_Intro.cpp
+++ cgsg2018/src/CGC2018_00_Intro/CGC2018_00_Intro.cpp
@@ -10,14 +10,21 @@ void Display()
    glutSwapBuffers();
 }
 
+void KeyboardCB(unsigned char key, int x, int y) {
+   switch (key) {
+      case 27:
+         exit(0);
+   }
+}
 
 int main(int argc, char* argv[]) {
    glutInit(&argc, argv);
    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
    glutInitWindowSize(800, 600);
    glutCreateWindow(PROJECT_NAME);
-   glutDisplayFunc(Display);
 
+   glutDisplayFunc(Display);
+   glutKeyboardFunc(KeyboardCB);
 
    glutMainLoop();
    return 0;
