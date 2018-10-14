5eb1f135a570f76ff72ad805e5316b6167f9b6f7
Igor Beliaev <slipstak2@yandex.ru>
2018-10-14 22:19:41 +0300

    Bitmap Raster Fonts - demo

---
+++ cgsg2018/src/CGC2018_01_Raster/CGC2018_01_Raster_Font.cpp
@@ -0,0 +1,107 @@
+// http://www.undocprint.org/formats/font_formats#fon_files
+// http://justsolve.archiveteam.org/wiki/FNT_(Windows_Font)
+// http://www.os2museum.com/files/docs/win10sdk/windows-1.03-sdk-prgref-1986.pdf
+
+// https://github.com/bson/enetcore/tree/9f8055bdb7a5d013e9e3905983db6c358d6bbc25/hardware/font maybe bad format
+#define _CRT_SECURE_NO_WARNINGS
+
+#include "GL/freeglut.h"
+#include <GL/glut.h>
+
+#include <iostream>
+#include <string>
+#include "fonts/Fnt.h"
+
+using namespace std;
+
+const int W = 800;
+const int H = 600;
+
+float buffer[H][W];
+
+fonts::Fnt_Ptr fntConsolas = fonts::Fnt::LoadFromFile("fonts/Consolas-20.fnt");
+fonts::Fnt_Ptr fntFkp = fonts::Fnt::LoadFromFile("fonts/fkpmedium-16.fnt");
+fonts::Fnt_Ptr fntPixelCarnage = fonts::Fnt::LoadFromFile("fonts/PixelCarnageMonospace-12.fnt");
+
+class GLintPoint {
+public:
+   GLint x, y;
+};
+
+void myInit() {
+   glClearColor(1.0, 1.0, 1.0, 1.0);      // background color = white
+   glColor3f(1.0, 0.0, 0.0);              // drawing color    = red
+   glPointSize(1);                        // point size       = 4
+   glMatrixMode(GL_PROJECTION);
+   glLoadIdentity();
+   gluOrtho2D(0, 800, 600, 0);
+}
+
+void myMouse(int button, int state, int x, int y) {
+   printf("button = %d; button = %d, x = %d; y = %d\n", button, state, x, y);
+}
+
+void myReshape(int width, int height) {
+   printf("width = %d; height=%d\n", width, height);
+}
+
+void drawDot(GLint x, GLint y) {
+   glBegin(GL_POINTS);
+   glVertex2i(x, y);
+   glEnd();
+}
+
+void drawCharacter(unsigned char c, fonts::Fnt_Ptr fnt, GLint x, GLint y) {
+   fnt->ScanBitmap(c, [x, y](unsigned short row, unsigned short col, bool isSetPixel) {
+      if (isSetPixel) {
+         drawDot(x + col, y + row);
+      }
+   });
+}
+void drawString(const string &s, fonts::Fnt_Ptr fnt, GLint x, GLint y) {
+   unsigned short h = fnt->characterHeight();
+   unsigned short w = fnt->characterWidth();
+
+   for (size_t i = 0; i < s.size(); ++i) {
+      drawCharacter(s[i], fnt, x + i * w, y);
+   }
+}
+
+void myDisplay(void) {
+   glClear(GL_COLOR_BUFFER_BIT);
+
+   int y = 10;
+
+   drawString("The quick brown fox jumps over the lazy dog. 0123456789", fntPixelCarnage, 10, y);
+   drawString("Jackdaws love my big sphinx of quartz. 0123456789", fntFkp, 10, y += 20);
+   drawString("The five boxing wizards jump quickly. 0123456789", fntConsolas, 10, y += 20);
+
+
+   glFlush();
+}
+
+void myKeyBoard(unsigned char key, int x, int y) {
+   switch (key) {
+   case 27:
+      exit(0);
+   }
+}
+
+
+int main(int argc, char* argv[]) {
+   glutInit(&argc, argv);
+   glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+   glutInitWindowSize(800, 600);
+   glutInitWindowPosition(100, 150);
+   glutCreateWindow(PROJECT_NAME);
+
+   glutDisplayFunc(myDisplay);
+   glutMouseFunc(myMouse);
+   glutKeyboardFunc(myKeyBoard);
+   glutReshapeFunc(myReshape);
+
+
+   myInit();
+   glutMainLoop();
+   return 0;
+}
---
+++ cgsg2018/src/utils/fonts/Fnt.h
@@ -0,0 +1,74 @@
+#include <memory>
+#include <functional>
+
+namespace fonts  {
+
+   class Fnt;
+   typedef std::shared_ptr<Fnt> Fnt_Ptr;
+
+   class Fnt
+   {
+#pragma pack(push, 1)
+      struct FntHeader {
+         unsigned short  dfVersion;
+         unsigned long   dfSize;
+         char            dfCopyright[60];
+         unsigned short  dfType;
+         unsigned short  dfPoints;
+         unsigned short  dfVertRes;
+         unsigned short  dfHorizRes;
+         unsigned short  dfAscent;
+         unsigned short  dfInternalLeading;
+         unsigned short  dfExternalLeading;
+         unsigned char   dfItalic;
+         unsigned char   dfUnderline;
+         unsigned char   dfStrikeOut;
+         unsigned short  dfWeight;
+         unsigned char   dfCharSet;
+         unsigned short  dfPixWidth;
+         unsigned short  dfPixHeight;
+         unsigned char   dfPitchAndFamily;
+         unsigned short  dfAvgWidth;
+         unsigned short  dfMaxWidth;
+         unsigned char   dfFirstChar;
+         unsigned char   dfLastChar;
+         unsigned char   dfDefaultChar;
+         unsigned char   dfBreakChar;
+         unsigned short  dfWidthBytes;
+         unsigned long   dfDevice;
+         unsigned long   dfFace;
+         unsigned long   dfBitsPointer;
+         unsigned long   dfBitsOffset;
+         unsigned char   dfReserved;
+         unsigned long   dfFlags;
+      };
+#pragma pack(pop)
+
+      typedef std::function<void(unsigned short x, unsigned short y, bool is_set_pixel)> scanFntVisitor;
+
+   public:
+      static Fnt_Ptr LoadFromFile(const char* file_name);
+      const char* name();
+      unsigned short characterWidth();
+      unsigned short characterHeight();
+
+      bool DrawTerminal(unsigned char c, unsigned char border='X', unsigned char background='.');
+      bool ScanBitmap(unsigned char c, scanFntVisitor visitor);
+      
+      ~Fnt() {
+         delete [] _file_content;
+      }
+   private:
+      Fnt(const char* file_content);
+      bool _scanBitmapWidth8(unsigned char c, scanFntVisitor visitor);      // for understanding format
+      bool _scanBitmapUniversal(unsigned char c, scanFntVisitor visitor);   // thanks: https://github.com/DisplayCore/FNT/blob/0b266707dd4ac14fd9f34ff9545a136ac0d0d861/FNT.cpp 
+
+      
+   private:
+      FntHeader*  _header;
+      const char* _font_table;
+
+      const char* _file_content;
+   };
+   
+}
